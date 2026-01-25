cbc25b742c920d8afa7ca91f7486a7f57e201637
changhyeok <dpkyle97@gmail.com>
2026-01-25 12:08:30 +0900

    chore: 모듈 이름 변경

--- Package.swift
+++ Package.swift
@@ -4,24 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "NetworkKit",
+    name: "NetworkBaseKit",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "NetworkKit",
-            targets: ["NetworkKit"]
+            name: "NetworkBaseKit",
+            targets: ["NetworkBaseKit"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkKit"
+            name: "NetworkBaseKit"
         ),
         .testTarget(
-            name: "NetworkKitTests",
-            dependencies: ["NetworkKit"]
+            name: "NetworkBaseKitTests",
+            dependencies: ["NetworkBaseKit"]
         ),
     ]
 )
